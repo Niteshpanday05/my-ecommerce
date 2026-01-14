dec121ab2ecadf0def1ee4ddfbc9d1da644827cb
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-14 15:44:59 +0545

    connecting stripe

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,4 +1,12 @@
-export default function CheckoutPage() {
+import { stripe } from "@/lib/stripe"
+
+export default async function CheckoutPage() {
+    const products = await stripe.products.list({
+        expand:["data.default_price"],
+        limit: 5,
+    })
+
+console.log(products);
     return(
         <div>
             this is checkout page
