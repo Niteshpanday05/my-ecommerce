c262db324b87411147da9f0c1ae3f26d971d3e02
Niteshpanday05 <eagleseye.np@gmail.com>
2026-01-18 19:49:51 +0545

    some changes in hero

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/components/Navbar";
-
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,12 +25,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <Navbar />
-        <main className=" p-4 m-2">
-          {children}
-        </main>
-      </body>
+      <body>
+  <Navbar />
+
+ <main className="pt-16">
+    {children}
+  </main>
+</body>
+
     </html>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,35 @@
+"use client";
+
 import Link from "next/link";
+import { ShoppingCart, User, Search, Menu, X } from "lucide-react";
+import { useState } from "react";
 
-export const Navbar = () => {
-  return (
-    <header className="sticky top-0 z-50 bg-white/80 backdrop-blur border-b">
-      <nav className="h-16">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:p-8 h-full flex items-center justify-between">
-          <div className="text-xl font-semibold">Brand</div>
+export default function Navbar() {
+ return(
+  <header className="sticky top-0 z-50 bg-white/80 backdrop-blur border-b">
+  <nav className="mx-auto max-w-7xl h-16 px-4 flex items-center justify-between">
+    
+    {/* LEFT */}
+    <div className="flex items-center  gap-2">
+      <span className="text-xl flex justify-end font-bold">Brand</span>
+    </div>
 
-          
-          <div className="hidden md:flex items-center gap-6">
-            <a href="#">Home</a>
-            <a href="#">Shop</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
-          </div>
 
-          {/* Mobile Menu Button */}
-          <button className="md:hidden">☰</button>
-        </div>
-      </nav>
-    </header>
-  );
-};
+    {/* CENTER */}
+    <ul className="hidden md:flex text-green-500 items-center gap-20">
+      <li>Home</li>
+      <li>Shop</li>
+      <li>About</li>
+    </ul>
+
+    {/* RIGHT */}
+    <div className="flex items-center gap-4">
+      <button>Login</button>
+      <button className="md:hidden">☰</button>
+    </div>
+
+  </nav>
+</header>
+
+ )
+}
